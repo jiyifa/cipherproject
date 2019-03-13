7b0d6af45368ce69c6897742387eb8a616daede5
jiyifa <jiyifa2017@126.com>
2019-03-13 23:24:45 +0800

    init

--- src/main/java/com/jiyifa/RSA.java
+++ src/main/java/com/jiyifa/RSA.java
@@ -2,11 +2,20 @@ package com.jiyifa;
 
 import org.apache.commons.codec.binary.Base64;
 
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 public class RSA {
+
+    private  static String rsaPublicKey = "";
+    private static String rsaPrivateKey = "";
+
     public static void main(String[] args) {
         initKey();
     }
@@ -19,8 +28,8 @@ public class RSA {
             //64的整倍数
             keyPairGenerator.initialize(512);
             KeyPair keyPair = keyPairGenerator.generateKeyPair();
-            String rsaPublicKey = Base64.encodeBase64String(keyPair.getPublic().getEncoded());
-            String rsaPrivateKey = Base64.encodeBase64String(keyPair.getPrivate().getEncoded());
+            rsaPublicKey = Base64.encodeBase64String(keyPair.getPublic().getEncoded());
+            rsaPrivateKey = Base64.encodeBase64String(keyPair.getPrivate().getEncoded());
             System.out.println("Public Key : " + rsaPublicKey);
             System.out.println("Private Key : " + rsaPrivateKey);
             System.out.println();
@@ -28,5 +37,62 @@ public class RSA {
             e.printStackTrace();
         }
 
+    }
+
+    /**
+     * 私钥加密，公钥解密
+     * @param src
+     * @return
+     */
+    public static String pubDecode(String src){
+        try {
+            X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(Base64.decodeBase64(rsaPublicKey));
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            PublicKey publicKey = keyFactory.generatePublic(x509EncodedKeySpec);
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.DECRYPT_MODE,publicKey);
+            byte[] result = Base64.decodeBase64(src);
+            result = cipher.doFinal(result);
+            return new String(result);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public static String priEncode(String src){
+        PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(Base64.decodeBase64(rsaPrivateKey));
+        try {
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            PrivateKey privateKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.ENCRYPT_MODE,privateKey);
+            byte[] result = cipher.doFinal(src.getBytes());
+            return Base64.encodeBase64String(result);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeySpecException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
